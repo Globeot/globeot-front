ff69d705fa50a763822f4be8938c79ff2bfcb9c1
Aileen050925 <Aileen050925@gmail.com>
2026-03-15 17:27:52 +0900

    학점 백분위 환산표 적용,계산로직 수정

---
+++ src/app/application-db/gpaData.ts
@@ -0,0 +1,23 @@
+export const gpaToPercentile: Record<string, number> = {};
+
+for (let i = 0; i <= 430; i++) {
+  const gpaStr = (i / 100).toFixed(2);
+  let val = 0;
+
+  if (i >= 400) {
+    val = 72 + ((i - 70) * 28) / 360;
+  } else if (i >= 370) {
+    val = 69 + ((i - 70) * 31) / 360;
+  } else if (i >= 330) {
+    val = 66 + ((i - 70) * 34) / 360;
+  } else if (i >= 300) {
+    val = 65 + ((i - 70) * 35) / 360;
+  } else if (i >= 270) {
+    val = 64 + ((i - 70) * 36) / 360;
+  } else {
+    val = 63 + ((i - 70) * 37) / 360;
+  }
+
+  const percentile = Math.round(val * 10) / 10;
+  gpaToPercentile[gpaStr] = percentile;
+}
