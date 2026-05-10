480585c46f70c48bb78982e88793eb16645f9874
pulipur10 <mindy040320@gmail.com>
2026-05-10 23:45:20 +0900

    엑세스 토큰 만료 시 오류 수정

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,12 +6,26 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
-
-  if (token) {
+if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("accessToken");
+
+      alert("로그인이 만료되었습니다. 다시 로그인해주세요.");
+
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
